2e9446dc5f52fbb3e6535755bdd1a5512b1901d8
amiltonxavier <amilton.jose@etic.co.ao>
2024-09-24 15:03:05 +0100

    add Not-found page

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,14 +1,19 @@
 import { createBrowserRouter as Browser } from "react-router-dom";
 import { Dashboard } from "../pages/Dashboard";
 import { Invited } from "../pages/Invited";
+import { NotFound } from "../pages/not-found";
 
 export const router = Browser([
-    {
-      path: "/",
-      element: <Dashboard />,
-    },
-    {
-      path: "/events/:id",
-      element: <Invited />
-    }
-  ]);
+  {
+    path: "/",
+    element: <Dashboard />,
+  },
+  {
+    path: "/events/:id",
+    element: <Invited />
+  },
+  {
+    path: "*",
+    element: <NotFound />
+  }
+]);
